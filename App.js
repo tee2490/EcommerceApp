68667418daf787463521efa284d94c06c236d9e9
tee2490@hotmail.com <tee2490@hotmail.com>
2024-05-29 08:29:51 +0700

    Navigation and Tab navigation

--- App.js
+++ App.js
@@ -1,14 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="HOME" component={HomeScreen} />
+        <Tab.Screen name="REORDER" component={HomeScreen} />
+        <Tab.Screen name="CART" component={HomeScreen} />
+        <Tab.Screen name="ACCOUNT" component={HomeScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   )
 }
 
 export default App
-
-const styles = StyleSheet.create({})
