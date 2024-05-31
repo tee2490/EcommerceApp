e7af127a8aaeea1d908639bae16324d1161e2ca7
tee2490@hotmail.com <tee2490@hotmail.com>
2024-05-31 14:57:17 +0700

    ProductDetailScreen-Gradient Background and Image

--- src/screen/ProductDetailsScreen.jsx
+++ src/screen/ProductDetailsScreen.jsx
@@ -1,18 +1,52 @@
-import { Platform, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, Platform, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { LinearGradient } from "expo-linear-gradient";
+import Header from "../components/Header";
+import { useRoute } from "@react-navigation/native";
 
 const ProductDetailsScreen = () => {
+  const route = useRoute();
+  const product = route.params.item;
+
   return (
     <View style={styles.container}>
-      <Text>ProductDetailsScreen</Text>
+      <LinearGradient
+        // Background Linear Gradient
+        colors={["rgba(20, 11, 178, 0.8)", "transparent"]}
+        style={styles.background}
+      />
+      <Header />
+
+      <View style={styles.imageContainer}>
+        <Image source={{ uri: product.image }} style={styles.coverImage} />
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default ProductDetailsScreen
+export default ProductDetailsScreen;
 
-const styles = StyleSheet.create({ container: {
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
     backgroundColor: "white",
     marginTop: Platform.OS == "android" ? 25 : 0,
-    padding: 20,
-  },})
+    padding: 10,
+  },
+  background: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    height: "100%",
+  },
+  imageContainer: {
+    height: 400,
+    width: "100%",
+    marginTop: 10,
+  },
+  coverImage: {
+    resizeMode: "cover",
+    flex: 1,
+  },
+});
