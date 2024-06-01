210cc535fe5bb4b91c73a9d1c76371905d9c0b26
tee2490@hotmail.com <tee2490@hotmail.com>
2024-06-01 15:42:43 +0700

    Context – Add Product To Cart Using AsyncStorage

--- src/Context/CartContext.js
+++ src/Context/CartContext.js
@@ -1,12 +1,28 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createContext, useState } from "react";
 
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
-  const [cartItems, setCartItems] = useState([1,2,3]);
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCartItem = async (item) => {
+    let cartItems = await AsyncStorage.getItem("cart");
+    cartItems = cartItems ? JSON.parse(cartItems) : [];
+    let isExist = cartItems.findIndex((cart) => cart.id === item.id);
+    
+    //ถ้ายังไม่มี
+    if (isExist === -1) {
+      cartItems.push(item);
+    //  calculateTotalPrice(cartItems);
+      setCartItems(cartItems);
+      await AsyncStorage.setItem("cart", JSON.stringify(cartItems));
+    }
+  };
 
   const value = {
     cartItems,
+    addToCartItem,
   };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
