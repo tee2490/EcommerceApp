c414aa769a09ad0b953f95c79b24121ec9274adb
tee2490@hotmail.com <tee2490@hotmail.com>
2024-06-01 16:26:14 +0700

    Context – loadCartItems

--- src/Context/CartContext.js
+++ src/Context/CartContext.js
@@ -1,20 +1,31 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState([]);
 
+  useEffect(() => {
+    loadCartItems();
+  }, []);
+
+  const loadCartItems = async () => {
+    await AsyncStorage.getItem("cart").then((cartItems) => {
+      cartItems = JSON.parse(cartItems);
+      setCartItems(cartItems);
+    });
+  };
+
   const addToCartItem = async (item) => {
     let cartItems = await AsyncStorage.getItem("cart");
     cartItems = cartItems ? JSON.parse(cartItems) : [];
     let isExist = cartItems.findIndex((cart) => cart.id === item.id);
-    
+
     //ถ้ายังไม่มี
     if (isExist === -1) {
       cartItems.push(item);
-    //  calculateTotalPrice(cartItems);
+      //  calculateTotalPrice(cartItems);
       setCartItems(cartItems);
       await AsyncStorage.setItem("cart", JSON.stringify(cartItems));
     }
