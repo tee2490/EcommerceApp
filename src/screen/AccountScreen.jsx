2d64b8672e5b708dab0025bdb2928abe463cbefa
tee2490@hotmail.com <tee2490@hotmail.com>
2024-06-06 15:48:35 +0700

    Formik & Yup -  Configuring the Validation Schema

--- src/screen/AccountScreen.jsx
+++ src/screen/AccountScreen.jsx
@@ -1,16 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 import * as yup from "yup";
 import { Formik } from "formik";
+
+const schema = yup.object().shape({
+  email: yup.string().required("กรุณากรอกข้อมูล").email("Invalid email"),
+  password: yup
+    .string()
+    .required("กรุณากรอกข้อมูล")
+    .min(8, "Password must contain at least 8 characters"),
+});
 
 const AccountScreen = () => {
   return (
     <View>
       <Text>AccountScreen</Text>
     </View>
-  )
-}
+  );
+};
 
-export default AccountScreen
+export default AccountScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
