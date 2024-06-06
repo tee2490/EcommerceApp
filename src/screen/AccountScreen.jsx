9a6f348ff288d7e0a8b2fdcafee0eda1a1f8573d
tee2490@hotmail.com <tee2490@hotmail.com>
2024-06-06 15:00:55 +0700

    React Hook Form & Yup – Css Style

--- src/screen/AccountScreen.jsx
+++ src/screen/AccountScreen.jsx
@@ -1,8 +1,9 @@
-import { TextInput, Button, StyleSheet, Text, View } from "react-native";
+import { TextInput, Button, StyleSheet, Text, View, Alert } from "react-native";
 import React from "react";
 import * as yup from "yup";
 import { useForm, Controller } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { LinearGradient } from "expo-linear-gradient";
 
 const schema = yup.object().shape({
   email: yup.string().required("กรุณากรอกข้อมูล").email("Invalid email"),
@@ -26,12 +27,18 @@ const AccountScreen = () => {
   });
 
   const onPressSend = (formData) => {
-    // Perform actions with the validated form data
+    Alert.alert(JSON.stringify(formData));
   };
 
   return (
-    <View>
-      <View>
+    <View style={styles.container}>
+      <LinearGradient
+        // Background Linear Gradient
+        colors={["rgba(9, 112, 24, 0.8)", "transparent"]}
+        style={styles.background}
+      />
+
+      <View style={styles.signupContainer}>
         <Controller
           control={control}
           rules={{
@@ -39,6 +46,7 @@ const AccountScreen = () => {
           }}
           render={({ field: { onChange, value } }) => (
             <TextInput
+              style={styles.textInput}
               value={value}
               onChangeText={onChange}
               placeholder="Email"
@@ -46,7 +54,9 @@ const AccountScreen = () => {
           )}
           name="email"
         />
-        {errors.email && <Text>{errors.email.message}</Text>}
+        {errors.email && (
+          <Text style={styles.errorText}>{errors.email.message}</Text>
+        )}
         <Controller
           control={control}
           rules={{
@@ -54,6 +64,7 @@ const AccountScreen = () => {
           }}
           render={({ field: { onChange, value } }) => (
             <TextInput
+              style={styles.textInput}
               value={value}
               onChangeText={onChange}
               placeholder="Password"
@@ -62,13 +73,52 @@ const AccountScreen = () => {
           )}
           name="password"
         />
-        {errors.password && <Text>{errors.password.message}</Text>}
+        {errors.password && (
+          <Text style={styles.errorText}>{errors.password.message}</Text>
+        )}
+
+        <Button title="Submit" onPress={handleSubmit(onPressSend)} />
       </View>
-      <Button title="Submit" onPress={handleSubmit(onPressSend)} />
     </View>
   );
 };
 
 export default AccountScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    padding: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  background: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    height: 300,
+  },
+  textInput: {
+    height: 40,
+    width: "100%",
+    marginBottom: 10,
+    backgroundColor: "white",
+    borderColor: "gray",
+    borderWidth: StyleSheet.hairlineWidth,
+    borderRadius: 10,
+  },
+  errorText: {
+    fontSize: 14,
+    color: "red",
+  },
+  signupContainer: {
+    width: "80%",
+    backgroundColor: "white",
+    padding: 20,
+    elevation: 10,
+    backgroundColor: "#e9f0ea",
+    borderRadius: 10,
+  },
+});
