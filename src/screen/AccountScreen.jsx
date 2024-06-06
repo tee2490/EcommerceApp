797634f82036e3e365cb9e71f2680f6c531beb18
tee2490@hotmail.com <tee2490@hotmail.com>
2024-06-06 08:23:36 +0700

    React Hook Form & Yup – Configuring the Validation Schema

--- src/screen/AccountScreen.jsx
+++ src/screen/AccountScreen.jsx
@@ -1,14 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const schema = yup.object().shape({
+  email: yup.string().required("กรุณากรอกข้อมูล").email("Invalid email"),
+  password: yup
+    .string()
+    .required("กรุณากรอกข้อมูล")
+    .min(8, "Password must contain at least 8 characters"),
+});
 
 const AccountScreen = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
   return (
     <View>
       <Text>AccountScreen</Text>
     </View>
-  )
-}
+  );
+};
 
-export default AccountScreen
+export default AccountScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
