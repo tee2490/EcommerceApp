0304809c434afc579d227804f1aab12c709ffc3f
tee2490@hotmail.com <tee2490@hotmail.com>
2024-06-06 15:33:03 +0700

    Formik & Yup -  Installation

--- src/screen/AccountScreen.jsx
+++ src/screen/AccountScreen.jsx
@@ -1,124 +1,16 @@
-import { TextInput, Button, StyleSheet, Text, View, Alert } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 import * as yup from "yup";
-import { useForm, Controller } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { LinearGradient } from "expo-linear-gradient";
-
-const schema = yup.object().shape({
-  email: yup.string().required("กรุณากรอกข้อมูล").email("Invalid email"),
-  password: yup
-    .string()
-    .required("กรุณากรอกข้อมูล")
-    .min(8, "Password must contain at least 8 characters"),
-});
+import { Formik } from "formik";
 
 const AccountScreen = () => {
-  const {
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(schema),
-    defaultValues: {
-      email: "",
-      password: "",
-    },
-  });
-
-  const onPressSend = (formData) => {
-    Alert.alert(JSON.stringify(formData));
-  };
-
   return (
-    <View style={styles.container}>
-      <LinearGradient
-        // Background Linear Gradient
-        colors={["rgba(9, 112, 24, 0.8)", "transparent"]}
-        style={styles.background}
-      />
-
-      <View style={styles.signupContainer}>
-        <Controller
-          control={control}
-          rules={{
-            required: true,
-          }}
-          render={({ field: { onChange, value } }) => (
-            <TextInput
-              style={styles.textInput}
-              value={value}
-              onChangeText={onChange}
-              placeholder="Email"
-            />
-          )}
-          name="email"
-        />
-        {errors.email && (
-          <Text style={styles.errorText}>{errors.email.message}</Text>
-        )}
-        <Controller
-          control={control}
-          rules={{
-            required: true,
-          }}
-          render={({ field: { onChange, value } }) => (
-            <TextInput
-              style={styles.textInput}
-              value={value}
-              onChangeText={onChange}
-              placeholder="Password"
-              secureTextEntry
-            />
-          )}
-          name="password"
-        />
-        {errors.password && (
-          <Text style={styles.errorText}>{errors.password.message}</Text>
-        )}
-
-        <Button title="Submit" onPress={handleSubmit(onPressSend)} />
-      </View>
+    <View>
+      <Text>AccountScreen</Text>
     </View>
-  );
-};
+  )
+}
 
-export default AccountScreen;
+export default AccountScreen
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "white",
-    padding: 20,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  background: {
-    position: "absolute",
-    left: 0,
-    right: 0,
-    top: 0,
-    height: 300,
-  },
-  textInput: {
-    height: 40,
-    width: "100%",
-    marginBottom: 10,
-    backgroundColor: "white",
-    borderColor: "gray",
-    borderWidth: StyleSheet.hairlineWidth,
-    borderRadius: 10,
-  },
-  errorText: {
-    fontSize: 14,
-    color: "red",
-  },
-  signupContainer: {
-    width: "80%",
-    backgroundColor: "white",
-    padding: 20,
-    elevation: 10,
-    backgroundColor: "#e9f0ea",
-    borderRadius: 10,
-  },
-});
+const styles = StyleSheet.create({})
